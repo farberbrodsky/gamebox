6b9879dc5a07a3e9f845b30aa2f4e59568ccbd7c
Misha Farber Brodsky <misha@farberbrodsky.com>
2025-05-15 22:17:48 +0300

    this is the start!

--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,24 @@
 const std = @import("std");
+const linux = @import("std.os.linux");
 
-/// aaaaa
-pub fn main() void {
-    std.debug.print("Hello, {s}!\n", .{"Zig Build"});
+const STACK_SIZE = 1024 * 1024;
+
+pub fn evil_twin(arg: usize) callconv(.c) u8 {
+    _ = arg;
+    return 3;
+}
+
+pub fn main() !void {
+    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
+    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+
+    // linux.mmap()
+    // linux.clone()
+}
+
+test "simple test" {
+    var list = std.ArrayList(i32).init(std.testing.allocator);
+    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
+    try list.append(42);
+    try std.testing.expectEqual(@as(i32, 42), list.pop());
 }
