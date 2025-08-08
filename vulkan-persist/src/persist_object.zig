7ff1afb83f10d080133448e9f99a2ec2132514ad
Misha Farber Brodsky <misha@farberbrodsky.com>
2025-08-08 22:12:54 +0300

    start work on persist objects - the general wrapper object

---
+++ vulkan-persist/src/persist_object.zig
@@ -0,0 +1,28 @@
+const std = @import("std");
+
+const PersistObjectType = enum {
+    CreateDevice,
+};
+
+const PersistObject = struct {
+    const Self = @This();
+
+    /// Membership of a doubly linked list, ordering objects chronologically
+    /// in some order consistent with threading happens-before boundaries.
+    list_node: std.DoublyLinkedList.Node,
+
+    /// A PersistObject represents some underlying object at runtime
+    underlying_pointer: usize,
+
+    /// Type of the object when created
+    type: PersistObjectType,
+
+    /// An optional creation data packet stored in the same allocation
+    creation_packet_size: usize = 0,
+
+    pub fn creation_packet(self: *Self) []u8 {
+        const base: [*]u8 = @ptrCast(self);
+        const offset = comptime @sizeOf(Self);
+        return base[offset .. offset + self.creation_packet_size];
+    }
+};
