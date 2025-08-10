d2e66d55272faf04445b352ca842afffc832b597
Misha Farber Brodsky <misha@farberbrodsky.com>
2025-08-10 23:42:03 +0300

    less than legitimate code but ok

--- vulkan-persist/tools/generate_vk_wrappers.zig
+++ vulkan-persist/tools/generate_vk_wrappers.zig
@@ -1,6 +1,45 @@
 const std = @import("std");
 const xml = @import("xml");
 
+fn readInsideElement(xmlReader: *xml.Reader, elementName: []const u8) !?xml.Reader.Node {
+    const node = try xmlReader.read();
+
+    if (node == .eof) {
+        // this element needs to end
+        return error.UnexpectedEndOfXml;
+    }
+
+    if (node == .element_end) {
+        if (std.mem.eql(u8, xmlReader.elementName(), elementName)) {
+            // this element ended
+            return null;
+        }
+    }
+
+    return node;
+}
+
+fn parseCommand(alloc: std.mem.Allocator, xmlReader: *xml.Reader) !void {
+    _ = .{ alloc };
+    std.debug.print("Parsing a command\n", .{});
+
+    for (0..xmlReader.attributeCount()) |i| {
+        const attribute_name = xmlReader.attributeNameNs(i);
+        std.debug.print("attrib '{s}'\n", .{attribute_name.local});
+    }
+
+    while (try readInsideElement(xmlReader, "command")) |childNode| {
+        switch (childNode) {
+            .eof => { fatal("Unexpected EOF\n", .{}); },
+            else => {},
+        }
+    }
+}
+
+const ElementParsers = std.StaticStringMap(*const fn (std.mem.Allocator, *xml.Reader) anyerror!void).initComptime(.{
+    .{ "command", parseCommand },
+});
+
 pub fn main() !void {
     var arena_state = std.heap.ArenaAllocator.init(std.heap.page_allocator);
     defer arena_state.deinit();
@@ -8,11 +47,11 @@ pub fn main() !void {
 
     const args = try std.process.argsAlloc(arena);
 
-    if (args.len != 3) fatal("wrong number of arguments", .{});
+    if (args.len != 3) fatal("wrong number of arguments\n", .{});
 
     const input_file_path = args[1];
     var input_file = std.fs.cwd().openFile(input_file_path, .{}) catch |err| {
-        fatal("unable to open input '{s}': {s}", .{ input_file_path, @errorName(err) });
+        fatal("unable to open input '{s}': {s}\n", .{ input_file_path, @errorName(err) });
     };
     defer input_file.close();
 
@@ -24,28 +63,20 @@ pub fn main() !void {
 
     const output_file_path = args[2];
     var output_file = std.fs.cwd().createFile(output_file_path, .{}) catch |err| {
-        fatal("unable to open output '{s}': {s}", .{ output_file_path, @errorName(err) });
+        fatal("unable to open output '{s}': {s}\n", .{ output_file_path, @errorName(err) });
     };
     defer output_file.close();
 
     while (true) {
-        const node = xmlReader.read() catch |err| switch (err) {
-            error.MalformedXml => {
-                const loc = xmlReader.errorLocation();
-                fatal("{d}:{d}: {s}", .{ loc.line, loc.column, @tagName(xmlReader.errorCode()) });
-            },
-            else => {
-                fatal("Unexpected error in XML reader", .{});
-            },
-        };
+        const node = xmlReader.read() catch |err| fatalXmlError(xmlReader, err);
 
         switch (node) {
             .eof => break,
             .element_start => {
-                // hello
-            },
-            .element_end => {
-                // world
+                const elementName = xmlReader.elementName();
+                if (ElementParsers.get(elementName)) |elementParser| {
+                    elementParser(arena, &xmlReader.reader) catch |err| fatalXmlError(xmlReader, err);
+                }
             },
             else => {},
         }
@@ -54,10 +85,25 @@ pub fn main() !void {
     try output_file.writeAll(
         \\pub const Person = struct {
         \\   age: usize = 18,
-        \\   name: []const u8 = "foo"        
+        \\   name: []const u8 = "foo"
         \\};
     );
     return std.process.cleanExit();
+}
+
+fn fatalXmlError(xmlReader: anytype, err: anyerror) noreturn {
+    switch (err) {
+        error.MalformedXml => {
+            const loc = xmlReader.errorLocation();
+            fatal("{d}:{d}: {s}", .{ loc.line, loc.column, @tagName(xmlReader.errorCode()) });
+        },
+        error.UnexpectedEndOfXml => {
+            fatal("Unexpected end of XML", .{});
+        },
+        else => {
+            fatal("Unexpected error in XML reader", .{});
+        },
+    }
 }
 
 fn fatal(comptime format: []const u8, args: anytype) noreturn {
